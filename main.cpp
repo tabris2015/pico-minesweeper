8726a94192958fc4ae3442ea9d30a38a2ba7fa68
Jose Laruta <eduardo.laruta@gmail.com>
2021-11-25 01:11:21 -0400

    fix format

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include "pins.h"
 
 int main() {
-	stdio_init_all();
-	gpio_init(LED_PIN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
+    stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
     MotorBts7960 motor1(M1_LPWM_PIN, M1_RPWM_PIN);
-	while(true) {
-		gpio_put(LED_PIN, 0);
+    while(true) {
+        gpio_put(LED_PIN, 0);
         for(float i = 0.0f; i < 1.0f; i+=0.01f){
             motor1.write(i);
             sleep_ms(20);
@@ -28,7 +28,5 @@ int main() {
             motor1.write(-i);
             sleep_ms(20);
         }
-
-
 	}
 }
